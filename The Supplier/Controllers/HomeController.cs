2e887dbb2633f2b9b272c17def2638d458530cfb
Munwini Zwane <MZwane@jhb.dvt.co.za>
2016-09-26 15:23:22 +0200

    New modified project added

---
+++ The Supplier/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace The_Supplier.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
